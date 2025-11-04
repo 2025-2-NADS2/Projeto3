721d00292db3776a92d4498df13585816897dbb1
Gustavo Moura <159397965+GuhMoura@users.noreply.github.com>
2025-11-04 09:21:31 -0300

    .
    .

--- documentos/Entrega 1/Programação Orientada a Objetos/POO/POO/obj/Debug/net8.0/InstitutoAlma.AssemblyInfo.cs
+++ documentos/Entrega 1/Programação Orientada a Objetos/POO/POO/obj/Debug/net8.0/InstitutoAlma.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("InstitutoAlma")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2adc736aa98f0a1a4e6c87fdea829832aa9e3204")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+efd96ce59c710095b2abfc57a8549db15f6cdfc4")]
 [assembly: System.Reflection.AssemblyProductAttribute("InstitutoAlma")]
 [assembly: System.Reflection.AssemblyTitleAttribute("InstitutoAlma")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
