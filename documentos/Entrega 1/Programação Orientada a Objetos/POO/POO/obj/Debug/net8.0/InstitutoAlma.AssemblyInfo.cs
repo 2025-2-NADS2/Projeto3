93f3d0641e009f9601ea15f6b78e34426e225338
Gustavo Moura <159397965+GuhMoura@users.noreply.github.com>
2025-11-04 16:36:32 -0300

    Visão Icons | Gráficos
    Melhoria na Visão de Receita - Histórico e doações com gráficos melhores visualmente.

--- documentos/Entrega 1/Programação Orientada a Objetos/POO/POO/obj/Debug/net8.0/InstitutoAlma.AssemblyInfo.cs
+++ documentos/Entrega 1/Programação Orientada a Objetos/POO/POO/obj/Debug/net8.0/InstitutoAlma.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("InstitutoAlma")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+721d00292db3776a92d4498df13585816897dbb1")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+71d728dfce1dec57116103a0af8c5ee4d7676a95")]
 [assembly: System.Reflection.AssemblyProductAttribute("InstitutoAlma")]
 [assembly: System.Reflection.AssemblyTitleAttribute("InstitutoAlma")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
